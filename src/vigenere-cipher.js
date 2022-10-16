e20793ffff82ca545c1d1c918578dffef448a2e0
AP-Est <108468235+AP-Est@users.noreply.github.com>
2022-10-17 01:05:21 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (stage) {
+    this.stage=stage;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error ('Incorrect arguments!')
+    }
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    let messageArray = message.toUpperCase().split('')
+    let arr1 = message.toUpperCase().replace(/[\s]/g,'').split('')
+    let arr2 = key.toUpperCase().repeat(Math.ceil(arr1.length/key.length)).split('')
+    let arr3 = []
+    for (let i = 0; i < arr1.length; i++) {
+      if (letters.includes(arr1[i])) {
+      let num_msg = letters.indexOf(arr1[i])  
+      let num_key = letters.indexOf(arr2[i])  
+      let resu = (num_msg + num_key) % 26      
+      arr3.push(letters[resu])
+      }
+      else {
+        arr3.push(arr1[i])
+      }
+    }
+    for (let i = 0; i < messageArray.length; i++) {
+      if (messageArray[i] == ' ') {
+        arr3.splice(i, 0, ' ')
+      }      
+    }
+    if (this.stage == false) {
+      return arr3.reverse().join('')
+    }
+    return arr3.join('')
   }
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error ('Incorrect arguments!')
+    }
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    let messageArray = message.toUpperCase().split('')
+    let arr1 = message.toUpperCase().replace(/[\s]/g,'').split('')
+    let arr2 = key.toUpperCase().repeat(Math.ceil(arr1.length/key.length)).split('')
+    let arr3 = []
+    for (let i = 0; i < arr1.length; i++) {
+      if (letters.includes(arr1[i])) {
+      let num_msg = letters.indexOf(arr1[i])  
+      let num_key = letters.indexOf(arr2[i])  
+      let resu = (((num_msg - num_key) % 26) + 26) % 26     
+      arr3.push(letters[resu])
+      }
+      else {
+        arr3.push(arr1[i])
+      }
+    }
+    for (let i = 0; i < messageArray.length; i++) {
+      if (messageArray[i] == ' ') {
+        arr3.splice(i, 0, ' ')
+      }      
+    }
+    if (this.stage == false) {
+      return arr3.reverse().join('')
+    }
+    return arr3.join('')
+  }  
 }
 
 module.exports = {
